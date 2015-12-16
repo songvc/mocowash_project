ce5e81793c5cb2e5bd595d6fbcf29b5fde6e7da8
Vincent Song <songvc@users.noreply.github.com>
2015-12-16 10:29:51 -0800

    jumbotron

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
-	$(".nav-menu-button").click(function(){
-		$(".nav-menu").slideToggle(400, function(){
-			$(this).toggleClass("nav-expanded").css("display","");
-		});
+	
+	$(".open-panel").click(function(){
+	  $("html").addClass("openNav");
 	});
+
+	$(".close-panel, #content").click(function(){  
+		$("html").removeClass("openNav");
+	})
+
 });
