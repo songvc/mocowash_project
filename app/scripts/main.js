4c59cbb2988f94b80c6ae2f9510a2805384f6472
Vincent Song <songvc@users.noreply.github.com>
2015-12-16 23:12:16 -0800

    features

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+	// Controller responsive off-canvas navbar
 	$(".open-panel").click(function(){
 	  $("html").addClass("openNav");
 	});
@@ -7,5 +8,17 @@ $(document).ready(function(){
 	$(".close-panel, #content").click(function(){
 		$("html").removeClass("openNav");
 	})
+
+	// Controller for Fixed navbar when scrolling
+	$(window).scroll({previousTop: 0}, function () {
+			var currentTop = $(window).scrollTop();
+
+			if (currentTop < this.previousTop) {
+				$("header").show();
+			} else {
+				$("header").hide();
+			}
+			this.previousTop = currentTop;
+		});
 
 });
