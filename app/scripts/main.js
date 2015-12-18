99c6a848c9750b2b877cef81d177aedd938865ee
Vincent Song <songvc@users.noreply.github.com>
2015-12-17 22:59:28 -0800

    svg icon in progress

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,7 +9,83 @@ $(document).ready(function(){
 		$("html").removeClass("openNav");
 	})
 
-	// Controller for Fixed navbar when scrolling
+	// Icon images
+	var clock = new Vivus('icon-1', {
+		file: '../icons/clock.svg',
+		type: 'delayed',
+		duration: 500,
+		animTimingFunction: Vivus.EASE
+	});
+
+	var water = new Vivus('icon-2', {
+		file: '../icons/water.svg',
+		type: 'delayed',
+		duration: 500,
+		animTimingFunction: Vivus.EASE
+	});
+
+	var money = new Vivus('icon-3', {
+		file: '../icons/money.svg',
+		type: 'delayed',
+		duration: 500,
+		animTimingFunction: Vivus.EASE
+	});
+
+	var icon4 = new Vivus('icon-4', {
+		file: '../icons/icon1.svg',
+		type: 'delayed',
+		duration: 500,
+		animTimingFunction: Vivus.EASE
+	});
+
+	var icon5 = new Vivus('icon-5', {
+		file: '../icons/icon2.svg',
+		type: 'delayed',
+		duration: 500,
+		animTimingFunction: Vivus.EASE
+	});
+
+	var icon6 = new Vivus('icon-6', {
+		file: '../icons/icon3.svg',
+		type: 'delayed',
+		duration: 500,
+		animTimingFunction: Vivus.EASE
+	});
+
+	var icon7 = new Vivus('icon-7', {
+		file: '../icons/icon4.svg',
+		type: 'delayed',
+		duration: 500,
+		animTimingFunction: Vivus.EASE
+	});
+
+	var icon8 = new Vivus('icon-8', {
+		file: '../icons/icon5.svg',
+		type: 'delayed',
+		duration: 500,
+		animTimingFunction: Vivus.EASE
+	});
+
+	var icon9 = new Vivus('icon-9', {
+		file: '../icons/icon6.svg',
+		type: 'delayed',
+		duration: 500,
+		animTimingFunction: Vivus.EASE
+	});
+
+
+	clock
+		.stop()
+		.reset()
+		.play(1)
+	water
+		.stop()
+		.reset()
+		.play(1)
+	money
+		.stop()
+		.reset()
+		.play(1)
 
 
 });
