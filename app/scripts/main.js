ce4a23b5650d77e87aee26fbf3fac7ce20114200
Vincent Song <songvc@users.noreply.github.com>
2015-12-17 18:09:30 -0800

    svg icons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,15 +10,6 @@ $(document).ready(function(){
 	})
 
 	// Controller for Fixed navbar when scrolling
-	$(window).scroll({previousTop: 0}, function () {
-			var currentTop = $(window).scrollTop();
 
-			if (currentTop < this.previousTop) {
-				$(".header").show();
-			} else {
-				$(".header").hide();
-			}
-			this.previousTop = currentTop;
-		});
 
 });
