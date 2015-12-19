37b2b99c15680f8895efc6ee3bcdc36c69da9adb
Vincent Song <songvc@users.noreply.github.com>
2015-12-18 17:36:20 -0800

    progress

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,85 +7,101 @@ $(document).ready(function(){
 
 	$(".close-panel, #content").click(function(){
 		$("html").removeClass("openNav");
+	});
+
+	$('.btn').click(function(e){
+		if($('.modal').is(":visible")){
+				 $('.modal').hide();
+		} else{
+				 $('.modal').show();
+		}
+	});
+
+	$('.close').click(function(e){
+		e.preventDefault();
+		$('.modal').hide();
 	})
 
 	// Icon images
-	var clock = new Vivus('icon-1', {
-		file: '../icons/clock.svg',
-		type: 'delayed',
-		duration: 500,
-		animTimingFunction: Vivus.EASE
-	});
-
-	var water = new Vivus('icon-2', {
-		file: '../icons/water.svg',
-		type: 'delayed',
-		duration: 500,
-		animTimingFunction: Vivus.EASE
-	});
-
-	var money = new Vivus('icon-3', {
-		file: '../icons/money.svg',
-		type: 'delayed',
-		duration: 500,
-		animTimingFunction: Vivus.EASE
-	});
-
-	var icon4 = new Vivus('icon-4', {
-		file: '../icons/icon1.svg',
-		type: 'delayed',
-		duration: 500,
-		animTimingFunction: Vivus.EASE
-	});
-
-	var icon5 = new Vivus('icon-5', {
-		file: '../icons/icon2.svg',
-		type: 'delayed',
-		duration: 500,
-		animTimingFunction: Vivus.EASE
-	});
-
-	var icon6 = new Vivus('icon-6', {
-		file: '../icons/icon3.svg',
-		type: 'delayed',
-		duration: 500,
-		animTimingFunction: Vivus.EASE
-	});
-
-	var icon7 = new Vivus('icon-7', {
-		file: '../icons/icon4.svg',
-		type: 'delayed',
-		duration: 500,
-		animTimingFunction: Vivus.EASE
-	});
-
-	var icon8 = new Vivus('icon-8', {
-		file: '../icons/icon5.svg',
-		type: 'delayed',
-		duration: 500,
-		animTimingFunction: Vivus.EASE
-	});
-
-	var icon9 = new Vivus('icon-9', {
-		file: '../icons/icon6.svg',
-		type: 'delayed',
-		duration: 500,
-		animTimingFunction: Vivus.EASE
-	});
+	// $(".btn").click(function(){
+	// 	console.log("wow");
+	//
+	// });
 
 
-	clock
-		.stop()
-		.reset()
-		.play(1)
-	water
-		.stop()
-		.reset()
-		.play(1)
-	money
-		.stop()
-		.reset()
-		.play(1)
+	// var clock = new Vivus('icon-1', {
+	// 	file: '../icons/clock.svg',
+	// 	type: 'delayed',
+	// 	duration: 100,
+	// 	animTimingFunction: Vivus.EASE
+	// });
+	//
+	// var water = new Vivus('icon-2', {
+	// 	file: '../icons/water.svg',
+	// 	type: 'delayed',
+	// 	duration: 100,
+	// 	animTimingFunction: Vivus.EASE
+	// });
+	//
+	// var money = new Vivus('icon-3', {
+	// 	file: '../icons/money.svg',
+	// 	type: 'delayed',
+	// 	duration: 100,
+	// 	animTimingFunction: Vivus.EASE
+	// });
+	//
+	// var icon4 = new Vivus('icon-4', {
+	// 	file: '../icons/icon1.svg',
+	// 	type: 'delayed',
+	// 	duration: 100,
+	// 	animTimingFunction: Vivus.EASE
+	// });
+	//
+	// var icon5 = new Vivus('icon-5', {
+	// 	file: '../icons/icon2.svg',
+	// 	type: 'delayed',
+	// 	duration: 100,
+	// 	animTimingFunction: Vivus.EASE
+	// });
+	//
+	// var icon6 = new Vivus('icon-6', {
+	// 	file: '../icons/icon3.svg',
+	// 	type: 'delayed',
+	// 	duration: 100,
+	// 	animTimingFunction: Vivus.EASE
+	// });
+	//
+	// var icon7 = new Vivus('icon-7', {
+	// 	file: '../icons/icon4.svg',
+	// 	type: 'delayed',
+	// 	duration: 100,
+	// 	animTimingFunction: Vivus.EASE
+	// });
+	//
+	// var icon8 = new Vivus('icon-8', {
+	// 	file: '../icons/icon5.svg',
+	// 	type: 'delayed',
+	// 	duration: 100,
+	// 	animTimingFunction: Vivus.EASE
+	// });
+	//
+	// var icon9 = new Vivus('icon-9', {
+	// 	file: '../icons/icon6.svg',
+	// 	type: 'delayed',
+	// 	duration: 100,
+	// 	animTimingFunction: Vivus.EASE
+	// });
+
+
+
+	// water
+	// 	.stop()
+	// 	.reset()
+	// 	.play(1)
+	// money
+	// 	.stop()
+	// 	.reset()
+	// 	.play(1)
 
 
 });
