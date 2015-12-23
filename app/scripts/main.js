5b25ce6dd66660e2464ccd5c46055468e80b3963
Vincent Song <songvc@users.noreply.github.com>
2015-12-22 22:05:54 -0800

    more iteration on colors and buttons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,6 +10,19 @@ $(document).ready(function(){
 		$('html').removeClass('openNav');
 	});
 
+	// $(window).on('scroll', function() {
+	// 	var scrollTop = $(this).scrollTop();
+	// 	console.log(scrollTop);
+	//
+	// 	$('header').each(function() {
+	// 		var topDistance = $(this).offset().top;
+	//
+	// 		if ( (topDistance+100) < scrollTop ) {
+	// 		    alert( $(this).text() + ' was scrolled to the top' );
+	// 		}
+	// 	});
+	// });
+
 	// wiring buttons with modal
 	$('.btn').click(function(e){
 		e.preventDefault();
