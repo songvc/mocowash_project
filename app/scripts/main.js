079e64bdf41677bdade954ef3193167c4daa044c
Vincent Song <songvc@users.noreply.github.com>
2016-01-07 17:16:13 -0800

    navbar overhaul

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,13 +9,13 @@ $(document).ready(function(){
 	var $navlist	=		$('.nav-list');
 	var $openpanel = $('.open-panel');
 
-	$openpanel.click(function(){
-		$html.addClass('openNav');
-	});
-
-	$('.close-panel, #content').click(function(){
-		$html.removeClass('openNav');
-	});
+	// $openpanel.click(function(){
+	// 	$html.addClass('openNav');
+	// });
+	//
+	// $('.close-panel, #content').click(function(){
+	// 	$html.removeClass('openNav');
+	// });
 
 	// wiring buttons with modal
 	$('.btn').click(function(e){
@@ -32,31 +32,31 @@ $(document).ready(function(){
 		$modal.hide();
 	});
 
-	$window.scroll(function(){
-		if ($window.scrollTop() > 10){
-			$header.css({
-				'background-color': 'white',
-				'box-shadow': '0 .125rem .25rem rgba(51,61,71,.4)',
-				'background-image': 'none'
-			});
-
-			$navlist.css({
-					'color': 'black'
-			});
-
-		} else {
-			$header.css({
-				'background-color': 'transparent',
-				'box-shadow': 'none',
-				'background-image': 'linear-gradient(to bottom,rgba(25,25,25,0.95),transparent)'
-			});
-
-			$navlist.css({
-				'color': 'white'
-			});
-		}
-
-	});
+	// $window.scroll(function(){
+	// 	if ($window.scrollTop() > 10){
+	// 		$header.css({
+	// 			'background-color': 'white',
+	// 			'box-shadow': '0 .125rem .25rem rgba(51,61,71,.4)',
+	// 			'background-image': 'none'
+	// 		});
+	//
+	// 		$navlist.css({
+	// 				'color': 'black'
+	// 		});
+	//
+	// 	} else {
+	// 		$header.css({
+	// 			'background-color': 'transparent',
+	// 			'box-shadow': 'none',
+	// 			'background-image': 'linear-gradient(to bottom,rgba(25,25,25,0.95),transparent)'
+	// 		});
+	//
+	// 		$navlist.css({
+	// 			'color': 'white'
+	// 		});
+	// 	}
+	//
+	// });
 
 
 
