b6eef3e932499bea70233362ae258a56d174a93c
Vincent Song <songvc@users.noreply.github.com>
2016-01-09 01:04:26 -0800

    working on ribbons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,20 +2,15 @@
 
 $(document).ready(function(){
 
-	var $window 	= 	$(window);
-	var	$html		 	= 	$('html');
-	var $header 	= 	$('header');
-	var $modal		= 	$('.modal');
-	var $navlist	=		$('.nav-list');
-	var $openpanel = $('.open-panel');
+	var $window = $(window);
+	var $nav = $('nav');
+	var $modal = $('.modal');
+	var $servicebox = $('.service-box');
+	var $serviceprice = $('.service-price');
+	var $pull = $('#pull');
+	var	$menu = $('nav ul');
+	// var	menuHeight = $menu.height();
 
-	// $openpanel.click(function(){
-	// 	$html.addClass('openNav');
-	// });
-	//
-	// $('.close-panel, #content').click(function(){
-	// 	$html.removeClass('openNav');
-	// });
 
 	// wiring buttons with modal
 	$('.btn').click(function(e){
@@ -32,31 +27,59 @@ $(document).ready(function(){
 		$modal.hide();
 	});
 
-	// $window.scroll(function(){
-	// 	if ($window.scrollTop() > 10){
-	// 		$header.css({
-	// 			'background-color': 'white',
-	// 			'box-shadow': '0 .125rem .25rem rgba(51,61,71,.4)',
-	// 			'background-image': 'none'
-	// 		});
-	//
-	// 		$navlist.css({
-	// 				'color': 'black'
-	// 		});
-	//
-	// 	} else {
-	// 		$header.css({
-	// 			'background-color': 'transparent',
-	// 			'box-shadow': 'none',
-	// 			'background-image': 'linear-gradient(to bottom,rgba(25,25,25,0.95),transparent)'
-	// 		});
-	//
-	// 		$navlist.css({
-	// 			'color': 'white'
-	// 		});
-	// 	}
-	//
-	// });
+
+	$pull.on('click', function(e) {
+		e.preventDefault();
+		$menu.slideToggle();
+	});
+
+	$(window).resize(function(){
+    var w = $(window).width();
+    if(w > 320 && $menu.is(':hidden')) {
+      $menu.removeAttr('style');
+    }
+	});
+
+
+	// toggle for service & show price
+	$servicebox.click(function(e){
+		e.preventDefault();
+		$(this).addClass('hidden');
+	});
+
+	$serviceprice.click(function(e){
+		e.preventDefault();
+		// console.log("clicked" + $(this));
+		// console.log($(this).prev());
+		$(this).prev().removeClass("hidden");
+	});
+
+	// fixed navbar animation
+	$window.scroll(function(){
+		if ($window.scrollTop() > 10){
+			$nav.css({
+				'background-color': 'white',
+				'box-shadow': '0 .125rem .25rem rgba(51,61,71,.4)',
+				'background-image': 'none'
+			});
+
+			$nav.css({
+					'color': 'black'
+			});
+
+		} else {
+			$nav.css({
+				'background-color': 'transparent',
+				'box-shadow': 'none',
+				'background-image': 'linear-gradient(to bottom,rgba(25,25,25,0.95),transparent)'
+			});
+
+			$nav.css({
+				'color': 'white'
+			});
+		}
+
+	});
 
 
 
